ce3e1e1a86943c1b863faf45429793331cffa068
Bran <46734495+RealBrandonChen@users.noreply.github.com>
2021-07-15 17:31:41 +0800

    Update RegistrationKey.cs

--- RegistrationKey.cs
+++ RegistrationKey.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEditor;
 
 using SKM.V3;
 using SKM.V3.Methods;
 using SKM.V3.Models;
 
+using DeviceId;
+
 public class RegistrationKey : MonoBehaviour
 {
+
     //// Start is called before the first frame update
     private void Start()
     {
         // We recommend to review https://help.cryptolens.io/getting-started/unity for common errors and tips.
-        var RSAPubKey = "<RSAKeyValue><Modulus>******</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
-        var auth = "WyIyMjcxMjQ1IiwiNjV6cWN0VW******QXhUQml2UEt4NUNVY3dIdit4SjkzUXdob0JlbSJd";
-        Helpers.WindowsOnly = true;
-        var machineCode = Helpers.GetMachineCode();
-        //var machineCode = "default";
+        var RSAPubKey = "<RSAKeyValue><Modulus>YourPublicKeyHere</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+        var auth = "Your Auth Token";
+        var machineCode = new DeviceIdBuilder().AddMachineName().ToString();
+        //var machineCode = Helpers.GetMachineCodePI();
         var result = Key.Activate(token: auth, productId: 11605, key: "CTRXZ-AQECF-VBSWR-PXNFF", machineCode: machineCode);
     
         if (result == null || result.Result == ResultType.Error)
@@ -26,15 +29,20 @@ public class RegistrationKey : MonoBehaviour
             // (eg. the limit of activated devices was achieved)
             Debug.Log(result?.Message);
             Debug.Log("The license does not work.");
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+			Application.Quit();
+#endif
         }
         else
         {
             // obtaining the license key (and verifying the signature automatically).
             var license = LicenseKey.FromResponse(RSAPubKey, result);
+            
             // make sure to set Max Number of Machines (https://help.cryptolens.io/faq/index#maximum-number-of-machines)
             // if it's set to zero, please remove "!Helpers.IsOnRightMachinePI(license)".
-            
-            if (license == null || !Helpers.IsOnRightMachinePI(license))
+            if (license == null /*|| !Helpers.IsOnRightMachine(license)*/)
             {
                 Debug.Log("The license cannot be used on this device.");
                 return;
